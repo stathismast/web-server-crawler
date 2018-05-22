9a5af5b55278a2ecb60861b519d182eff1a6e4c3
stathismast <Stathis@LapBookAir.localdomain>
2018-05-22 19:36:57 +0300

    Cleaner code

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -2,6 +2,9 @@
 
 char line[4096]; //Used to read the http header line by line
 
+char * host;
+int port;
+
 Queue * queue;
 
 int getNextLine(int fd){
@@ -22,87 +25,94 @@ int findQuotations(char * str){
     return offset;
 }
 
-void sendHttpRequest(char * request, int argc, char *argv[]){
-    int port, sock; unsigned int serverlen;
-       struct sockaddr_in server;
-       struct sockaddr *serverptr;
-       struct hostent *rem;
-       if (argc < 3) {     /* Are server's host name and port number given? */
-          printf("Please give host name and port number\n"); exit(1); }
-       if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) { /* Create socket */
-          perror("socket"); exit(1); }
-       if ((rem = gethostbyname(argv[1])) == NULL) { /* Find server address */
-          perror("gethostbyname"); exit(1); }
-       port = atoi(argv[2]);              /* Convert port number to integer */
-       server.sin_family = PF_INET;                      /* Internet domain */
-       bcopy((char *) rem -> h_addr, (char *) &server.sin_addr,
-             rem -> h_length);
-       server.sin_port = htons(port); /* Server's Internet address and port */
-       serverptr = (struct sockaddr *) &server;
-       serverlen = sizeof server;
-       if (connect(sock, serverptr, serverlen) < 0) { /* Request connection */
-          perror("connect"); exit(1); }
-       // printf("Requested connection to host %s port %d\n", argv[1], port);
+void sendHttpRequest(char * request){
+    int sock; unsigned int serverlen;
+    struct sockaddr_in server;
+    struct sockaddr *serverptr;
+    struct hostent *rem;
 
+    if((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0){ /* Create socket */
+        perror("socket"); exit(1);
+    }
+    if((rem = gethostbyname(host)) == NULL){ /* Find server address */
+        perror("gethostbyname"); exit(1);
+    }
 
+    server.sin_family = PF_INET;                      /* Internet domain */
+    bcopy((char *) rem -> h_addr, (char *) &server.sin_addr, rem -> h_length);
+    server.sin_port = htons(port); /* Server's Internet address and port */
+    serverptr = (struct sockaddr *) &server;
+    serverlen = sizeof server;
 
-          // bzero(buf, sizeof buf);                      /* Initialize buffer */
-          // printf("Give input string: ");
-          // fgets(buf, sizeof buf, stdin);         /* Read message from stdin */
-          // strcpy(buf, request);
-          // buf[strlen(buf)-1] = '\0';            /* Remove newline character */
-          if (write(sock, request, strlen(request)) < 0) {           /* Send message */
-             perror("write"); exit(1); }
+    if(connect(sock, serverptr, serverlen) < 0){
+        perror("connect"); exit(1);
+    }
 
-          ssize_t bytesRead;
+    if(write(sock, request, strlen(request)) < 0){
+        perror("write"); exit(1);
+    }
 
-          int contentLength = 0;
-          do {
-            getNextLine(sock);
-            // printf("line: -%s-\n", line);
-            if(strstr(line,"Content-Length:") != NULL){
-                strtok(line," ");
-                contentLength = atoi(strtok(NULL," "));
-                // printf("CONTENT LENGTH IS %d\n", contentLength);
-            }
-        } while(strcmp(line,"\n") != 0);
+    //Read HTTP header and store content length
+    int contentLength = 0;
+    do {
+        getNextLine(sock);
+        if(strstr(line,"Content-Length:") != NULL){
+            strtok(line," ");
+            contentLength = atoi(strtok(NULL," "));
+        }
+    } while(strcmp(line,"\n") != 0);
 
-          char * buffer = malloc(contentLength);
-          if (bytesRead = read(sock, buffer, contentLength) < 0) {         /* Receive message */
-             perror("read"); exit(1); }
-          // printf("Read string:\n%s", buffer);
+    //Read file (by now we know the file's size)
+    char * buffer = malloc(contentLength);
+    if(read(sock, buffer, contentLength) < 0){         /* Receive message */
+        perror("read"); exit(1);
+    }
 
-          char * htmlLink = buffer;
-          int offset = 0;
-          while((htmlLink = strstr(&htmlLink[offset],"<a ")) != NULL){
-              htmlLink = strstr(htmlLink,"/");
-              offset = findQuotations(htmlLink);
-              char * link = malloc(offset + 1);
-              strncpy(link,htmlLink,offset);
-              link[offset] = 0;
-              // printf("-%s-\n", link);
-              addToQueue(link,&queue);
-              free(link);
-          }
+    //Search file for other links and add them to the queue
+    char * htmlLink = buffer;
+    int offset = 0;
+    while((htmlLink = strstr(&htmlLink[offset],"<a href")) != NULL){
+        htmlLink = strstr(htmlLink,"/");
+        offset = findQuotations(htmlLink);
+        char * link = malloc(offset + 1);
+        strncpy(link,htmlLink,offset);
+        link[offset] = 0;
+        addToQueue(link,&queue);
+        free(link);
+    }
 
-          // printf("\nBytes read: %d\n",(int)bytesRead);
-
-          free(buffer);
-       close(sock);
+    free(buffer);
+    close(sock);
 
 }
 
+void manageArguments(int argc, char *argv[]){
+    if(argc < 3){
+        printf("Please give host name and port number\n"); exit(1);
+    }
+    host = argv[1];
+    port = atoi(argv[2]);
+}
+
+char * createRequest(char * fileName){
+    char * request = malloc(strlen(fileName) + 5);
+    bzero(request, strlen(fileName) + 5);
+    strcpy(request,"GET ");
+    strcat(request,fileName);
+    return request;
+}
+
 int main(int argc, char *argv[]){
-    queue = NULL;
+
+    manageArguments(argc,argv);
+
+    queue = NULL;   //Initialize queue to NULL
     addToQueue("/site2/page7_16864.html",&queue);
 
     Queue * node = queue;
     while(node != NULL){
-        char * request = malloc(strlen(node->fileName) + 5);
-        bzero(request, strlen(node->fileName) + 5);
-        strcpy(request,"GET ");
-        strcat(request,node->fileName);
-        sendHttpRequest(request,argc,argv);
+        char * request = createRequest(node->fileName);
+        sendHttpRequest(request);
         free(request);
         node = node->next;
     }
